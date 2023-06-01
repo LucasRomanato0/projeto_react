a82383693fe1d49be9610e29600f37004b700612
lucas.romanato <lucas.romanato@mindlog.com.br>
2023-06-01 11:58:17 -0300

    componentizacao e renderizacao

--- primeiro_projeto/src/App.js
+++ primeiro_projeto/src/App.js
@@ -1,50 +1,14 @@
 import React, { Component } from "react";
+import Membro from "./components/Membro";
 
-class Equipe extends Component {
+class App extends Component {
   render() {
     return (
       <div>
-        <Sobre
-          nome={this.props.nome}
-          cargo={this.props.cargo}
-          idade={this.props.idade}
-        />
-        <Social />
-        <hr />
+        <Membro nome="Visitante" />
       </div>
     );
   }
-}
-
-class Sobre extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Olá, sou o(a) {this.props.nome}!</h2>
-        <h3>Cargo: {this.props.cargo}</h3>
-        <h3>Idade: {this.props.idade}</h3>
-      </div>
-    );
-  }
-}
-
-const Social = () => {
-  return (
-    <div>
-      <a>Facebook </a>
-      <a>LinkedIn </a>
-    </div>
-  );
-};
-
-function App() {
-  return (
-    <div>
-      <h1>Conheça nossa equipe:</h1>
-      <Equipe nome="Lucas" cargo="Programador" idade="21" />
-      <Equipe nome="Matheus" cargo="Suporte" idade="20" />
-    </div>
-  );
 }
 
 export default App;
