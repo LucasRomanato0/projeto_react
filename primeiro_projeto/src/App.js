d7a0b45d5f19ccf6db20b7e06d91b5760be9753f
lucas.romanato <lucas.romanato@mindlog.com.br>
2023-06-01 11:00:57 -0300

    class component

--- primeiro_projeto/src/App.js
+++ primeiro_projeto/src/App.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 
-const BemVindo = (props) => {
+class Equipe extends Component {
+  render() {
+    return (
+      <div>
+        <Sobre
+          nome={this.props.nome}
+          cargo={this.props.cargo}
+          idade={this.props.idade}
+        />
+        <Social />
+        <hr />
+      </div>
+    );
+  }
+}
+
+class Sobre extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Olá, sou o(a) {this.props.nome}!</h2>
+        <h3>Cargo: {this.props.cargo}</h3>
+        <h3>Idade: {this.props.idade}</h3>
+      </div>
+    );
+  }
+}
+
+const Social = () => {
   return (
     <div>
-      <h2>Bem-vindo(a) {props.nome}!</h2>
-      <h3>Tenho {props.idade} anos</h3>
+      <a>Facebook </a>
+      <a>LinkedIn </a>
     </div>
   );
 };
@@ -12,9 +40,9 @@ const BemVindo = (props) => {
 function App() {
   return (
     <div>
-      Olá Mundo!
-      <BemVindo nome="Lucas" idade="21" />
-      <BemVindo nome="Amanda" idade="19" />
+      <h1>Conheça nossa equipe:</h1>
+      <Equipe nome="Lucas" cargo="Programador" idade="21" />
+      <Equipe nome="Matheus" cargo="Suporte" idade="20" />
     </div>
   );
 }
