2a3917b34904d488028c6c3f4492b6fcbeb45cd8
lucas.romanato <lucas.romanato@mindlog.com.br>
2023-06-07 12:37:32 -0300

    continuando projeto filme_flix

--- prime_flix/src/pages/Filme/index.js
+++ prime_flix/src/pages/Filme/index.js
@@ -1,7 +1,97 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+import "./filme-info.css";
+
+import api from "../../services/api";
+
 function Filme() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: "978549951841bc006c66d82e0a93b3db",
+            language: "pt-BR",
+          },
+        })
+        .then((res) => {
+          setFilme(res.data);
+          console.log(res.data);
+          setLoading(false);
+        })
+        .catch(() => {
+          console.log("FILME NÃO ENCONTRADO");
+          navigate("/", { replace: true });
+          return;
+        });
+    }
+
+    loadFilme();
+
+    return () => {
+      console.log(" COMPONENTE FOI DESMONTADO");
+    };
+  }, [id, navigate]);
+
+  function salvarFilme() {
+    const minhaLista = localStorage.getItem("@primeflix");
+
+    // pega tudo que esta no localStorage (vazio ou nao)
+    let filmesSalvos = JSON.parse(minhaLista) || [];
+
+    const hasFilme = filmesSalvos.some(
+      (filmesSalvo) => filmesSalvo.id === filme.id
+    );
+
+    if (hasFilme) {
+      alert("ESSE FILME JÁ ESTÁ NA LISTA");
+      return; // para a aplicacao
+    }
+
+    filmesSalvos.push(filme);
+    localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
+    alert("FILME SALVO COM SUCESSO");
+  }
+
+  if (loading) {
+    return (
+      <div className="filme-info">
+        <h1>Carregando detalhes...</h1>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>BEM VINDO A PÁGINA DETALHES DO FILME</h1>
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
+        alt={filme.title}
+      />
+
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+      <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+      <div className="area-buttons">
+        <button onClick={salvarFilme}>Salvar</button>
+        <button>
+          <a
+            target="blank" // abre em outra guia
+            rel="external"
+            href={`https://youtube.com/results?search_query=${filme.title} Trailer`}
+          >
+            Trailer
+          </a>
+        </button>
+      </div>
     </div>
   );
 }
--- prime_flix/src/pages/Home/index.js
+++ prime_flix/src/pages/Home/index.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import api from "../../services/api";
+import { Link } from "react-router-dom";
+import "./home.css";
 
 //https://api.themoviedb.org/3/movie/now_playing?api_key=978549951841bc006c66d82e0a93b3db&language=pt-BR
 
 function Home() {
   const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadFilmes() {
@@ -16,15 +19,41 @@ function Home() {
         },
       });
 
-      console.log(response.data.results);
+      // slice(0, 10) -> mostra só os 10 primeiros
+      // console.log(response.data.results.slice(0, 10));
+
+      setFilmes(response.data.results.slice(0, 10));
+      setLoading(false);
     }
 
     loadFilmes();
   }, []);
 
+  //mostra enquanto a api estiver sendo carregada
+  if (loading) {
+    return (
+      <div className="loading">
+        <h2>Carregando filmes...</h2>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>BEM VINDO A HOME</h1>
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img
+                src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} // ta na documentacao da api
+                alt={filme.title}
+              />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 }
