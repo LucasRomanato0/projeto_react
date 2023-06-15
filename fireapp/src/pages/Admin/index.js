5577069ef076eae57db1d2c3365590eed1fd329a
LucasRomanato0 <lucasromanato34@gmail.com>
2023-06-15 11:11:59 -0300

    adicionando tarefas e pegando elas

--- fireapp/src/pages/Admin/index.js
+++ fireapp/src/pages/Admin/index.js
@@ -1,9 +1,111 @@
 import "./admin.css";
+import { useState, useEffect } from "react";
+
+import { auth, db } from "../../firebaseConnection";
+import { signOut } from "firebase/auth";
+
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  orderBy,
+  where,
+} from "firebase/firestore";
 
 export default function Admin() {
+  const [tarefaInput, setTarefaInput] = useState("");
+  const [user, setUser] = useState({});
+  const [tarefas, setTarefas] = useState([]);
+
+  useEffect(() => {
+    async function loadTarefas() {
+      const userDetail = localStorage.getItem("@detailUser");
+      setUser(JSON.parse(userDetail));
+
+      if (userDetail) {
+        const data = JSON.parse(userDetail);
+
+        //pegando as tarefas do usuario que esta logado
+        const tarefaRef = collection(db, "tarefas");
+        const q = query(
+          tarefaRef,
+          orderBy("created", "desc"),
+          where("autor", "==", data?.id)
+        );
+        const unsub = onSnapshot(q, (snapshot) => {
+          let lista = [];
+
+          snapshot.forEach((doc) => {
+            lista.push({
+              id: doc.id,
+              tarefa: doc.data().tarefa,
+              autor: doc.data().autor,
+            });
+          });
+
+          console.log(lista);
+          setTarefas(lista);
+        });
+      }
+    }
+
+    loadTarefas();
+  }, []);
+
+  async function handleRegister(e) {
+    e.preventDefault();
+
+    if (tarefaInput === "") {
+      alert("Digite uma tarefa...");
+      return;
+    }
+
+    await addDoc(collection(db, "tarefas"), {
+      tarefa: tarefaInput,
+      created: new Date(),
+      autor: user?.id,
+    })
+      .then(() => {
+        console.log("TAREFA REGISTRADA");
+        setTarefaInput("");
+      })
+      .catch((error) => {
+        console.log("ERRO AO REGISTRAR " + error);
+      });
+  }
+
+  async function handleLogout() {
+    await signOut(auth);
+  }
+
   return (
-    <div>
-      <h1>Pagina ADIMN</h1>
+    <div className="admin-container">
+      <h1>Minhas tarefas</h1>
+
+      <form className="form" onSubmit={handleRegister}>
+        <textarea
+          placeholder="Digite sua tarefa..."
+          value={tarefaInput}
+          onChange={(e) => setTarefaInput(e.target.value)}
+        />
+
+        <button className="btn-register" type="submit">
+          Registrar tarefa
+        </button>
+      </form>
+
+      <article className="list">
+        <p>Estudar JavaScript e React.js</p>
+        <div>
+          <button>Editar</button>
+          <button className="btn-concluir">Concluir</button>
+        </div>
+      </article>
+
+      <button className="btn-logout" onClick={handleLogout}>
+        Sair
+      </button>
     </div>
   );
 }
