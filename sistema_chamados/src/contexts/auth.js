bc6d9fd1d06524589196ad4ae695efe2769e8882
LucasRomanato0 <lucasromanato34@gmail.com>
2023-06-21 09:13:47 -0300

    cadastrando usuario

--- sistema_chamados/src/contexts/auth.js
+++ sistema_chamados/src/contexts/auth.js
@@ -1,14 +1,48 @@
 import { useState, createContext, useEffect } from "react";
+import { auth, db } from "../services/firebaseConnection";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
+  const [loadingAuth, setLoadingAuth] = useState(false);
 
   function signIn(email, passowrd) {
     console.log(email);
     console.log(passowrd);
     alert("LOGADO COM SUCESSO");
+  }
+
+  // Cadastrar um novo user
+  async function signUp(email, passowrd, name) {
+    setLoadingAuth(true);
+
+    await createUserWithEmailAndPassword(auth, email, passowrd)
+      .then(async (value) => {
+        let uid = value.user.uid;
+
+        await setDoc(doc(db, "users", uid), {
+          nome: name,
+          avatarUrl: null,
+        }).then(() => {
+          let data = {
+            uid: uid,
+            nome: name,
+            email: value.user.email,
+            avatarUrl: null,
+          };
+
+          setUser(data);
+
+          setLoadingAuth(false);
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoadingAuth(false);
+      });
   }
 
   return (
@@ -17,6 +51,8 @@ function AuthProvider({ children }) {
         signed: !!user, // !! converte pra bool, se não tiver nada é false
         user,
         signIn,
+        signUp,
+        loadingAuth,
       }}
     >
       {children}
